782cb1654d89c79de230311c5a220a8e6cea9ba8
damoliveira <danieldeandrade21@gmail.com>
2025-05-13 20:39:09 -0300

    Intervalo da aula

---
+++ dia02/exercicios/04.py
@@ -0,0 +1,8 @@
+#Faça um programa que calcule a raiz quadrada
+# de um número e exiba o resultado.
+# %%
+
+numeroRaiz =input("Digite um número inteiro para saber a sua raiz quadrada: ")
+
+print( "A Raiz quadrada de ",numeroRaiz, " é ",int(numeroRaiz)**(1/2))
+# %%
---
+++ dia02/exercicios/05.py
@@ -0,0 +1,7 @@
+#Faço um programa que exiba 
+# o dobro de um número inserido pelo usuário.
+
+# %%
+numero = input("Digite um número inteiro para saber o seu dobro: ")
+print("O dobro de ", numero, " é ", int(numero) * 2)
+# %%
